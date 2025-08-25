da0e0b178d586525f76bb7c3c9e661a0770101c0
Cron3x <cron3x.exe@gmail.com>
2025-08-25 18:05:39 +0200

    cleaned

--- src/loaders/kra.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-use std::fs;
-
-pub(super) fn unpack(mut archive: zip::ZipArchive<fs::File>) {
-    for i in 0..archive.len(){
-        let inner = archive.by_index(i).unwrap();
-        println!("\tName: {}", inner.name());
-    }
-}
